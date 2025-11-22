c9243fdcfada3727d3cc9e402dd49debde15acb6
bluware-dev <bluware.contact@gmail.com>
2025-11-22 13:27:41 -0300

    feat(features): implement page sections (Hero, About, Services, Experience, Showcase, Contact, Layout)

---
+++ src/components/features/index.ts
@@ -0,0 +1,17 @@
+// src/components/features/index.ts
+
+import AboutComponent from '@/components/features/About.astro';
+import ContactComponent from '@/components/features/Contact.astro';
+import ExperienceComponent from '@/components/features/Experience.astro';
+import HeroComponent from '@/components/features/Hero.astro';
+import LayoutComponent from '@/components/features/Layout.astro';
+import ServicesComponent from '@/components/features/Services.astro';
+import ShowcaseComponent from '@/components/features/Showcase.astro';
+
+export const About = AboutComponent;
+export const Contact = ContactComponent;
+export const Experience = ExperienceComponent;
+export const Hero = HeroComponent;
+export const Layout = LayoutComponent;
+export const Services = ServicesComponent;
+export const Showcase = ShowcaseComponent;
